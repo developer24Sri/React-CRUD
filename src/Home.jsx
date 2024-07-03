15999975a795b0610a6ba2d1ff31a4454d8cde0a
developer24Sri <88076621+developer24Sri@users.noreply.github.com>
2024-07-03 18:56:40 +0530

    react-crud project with router and axios-http methods

---
+++ src/Home.jsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  function handleDelete(id) {
+    const confirmDelete = window.confirm("Would you like to delete?");
+    if (confirmDelete) {
+      axios
+        .delete(`http://localhost:3000/users/${id}`)
+        .then(() => {
+          setData(data.filter((item) => item.id !== id));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center align-items-center min-vh-100">
+          <div className="col-lg-10">
+            <h1 className="text-center mb-4">List of Users</h1>
+            <div className="card shadow">
+              <div className="card-header d-flex justify-content-end">
+                <Link to="/create" className="btn btn-success">
+                  Add +
+                </Link>
+              </div>
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table className="table table-striped">
+                    <thead className="table-info">
+                      <tr>
+                        <th>Name</th>
+                        <th>E-mail</th>
+                        <th>Phone</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data.map((d) => (
+                        <tr key={d.id}>
+                          <td>{d.name}</td>
+                          <td>{d.email}</td>
+                          <td>{d.phone}</td>
+                          <td>
+                            <Link
+                              to={`/read/${d.id}`}
+                              className="btn btn-sm btn-info me-2"
+                            >
+                              Read
+                            </Link>
+                            <Link
+                              to={`/update/${d.id}`}
+                              className="btn btn-sm btn-primary me-2"
+                            >
+                              Edit
+                            </Link>
+                            <button
+                              onClick={() => handleDelete(d.id)}
+                              className="btn btn-sm btn-danger"
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/Update.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Update() {
+  const { id } = useParams();
+  const [values, setvalues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/users/` + id)
+      .then((res) => {
+        setvalues(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  function handleUpdate(e) {
+    e.preventDefault();
+    axios
+      .put("http://localhost:3000/users/" + id, values)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  }
+  return (
+    <>
+      <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+        <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+          <h1>Update User</h1>
+          <form onSubmit={handleUpdate}>
+            <div className="mb-2">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                name="name"
+                defaultValue={values.name}
+                className="form-control"
+                placeholder="Enter Name"
+                onChange={(e) => setvalues({ ...values, name: e.target.value })}
+              />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="email">E-mail:</label>
+              <input
+                type="email"
+                name="email"
+                defaultValue={values.email}
+                className="form-control"
+                placeholder="Enter E-mail"
+                onChange={(e) =>
+                  setvalues({ ...values, email: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phone">Phone:</label>
+              <input
+                type="tel"
+                name="phone"
+                defaultValue={values.phone}
+                className="form-control"
+                placeholder="Enter phone"
+                onChange={(e) =>
+                  setvalues({ ...values, phone: e.target.value })
+                }
+              />
+            </div>
+            <button className="btn btn-success" type="submit">
+              Update
+            </button>
+            <Link to="/" className="btn btn-primary ms-3">
+              Back
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
